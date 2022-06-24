263e005f07addecd231cbb0146d02eb5c0efc053
Mohit <mohitkoley5@gmail.com>
2022-06-24 19:36:22 +0530

    AppLifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -28,94 +27,31 @@ void main() {
 
 const String url = "https://images8.alphacoders.com/124/thumb-1920-1242638.jpg";
 
-enum Action { rotateLeft, rotateRight, lessVisible, moreVisible }
-
-class State {
-  final double alpha;
-  final double rotation;
-
-  State({required this.alpha, required this.rotation});
-
-  State.zero()
-      : alpha = 1.0,
-        rotation = 0.0;
-
-  rotateRight() => State(alpha: alpha, rotation: rotation + 10.0);
-
-  rotateLeft() => State(alpha: alpha, rotation: rotation - 10.0);
-
-  increaseAlpha() => State(alpha: min(alpha + 0.1, 1.0), rotation: rotation);
-
-  decreaseAlpha() => State(alpha: max(alpha - 0.1, 0.0), rotation: rotation);
-}
-
-State reducer(State oldState, Action? action) {
-  switch (action) {
-    case Action.lessVisible:
-      return oldState.decreaseAlpha();
-    case Action.moreVisible:
-      return oldState.increaseAlpha();
-    case Action.rotateLeft:
-      return oldState.rotateLeft();
-    case Action.rotateRight:
-      return oldState.rotateRight();
-    default:
-      return oldState;
-  }
-}
-
 class HomePage extends HookWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final store = useReducer<State, Action?>(reducer,
-        initialState: State.zero(), initialAction: null);
-
+    final lifecycle = useAppLifecycleState();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Hook Example"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                TextButton(
-                    onPressed: () {
-                      store.dispatch(Action.rotateLeft);
-                    },
-                    child: const Text("Rotate Left")),
-                TextButton(
-                    onPressed: () {
-                      store.dispatch(Action.rotateRight);
-                    },
-                    child: const Text("Rotate Right")),
-                TextButton(
-                    onPressed: () {
-                      store.dispatch(Action.moreVisible);
-                    },
-                    child: const Text("+ Alpha")),
-                TextButton(
-                    onPressed: () {
-                      store.dispatch(Action.lessVisible);
-                    },
-                    child: const Text("- Alpha")),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Opacity(
-              opacity: store.state.alpha,
-              child: RotationTransition(
-                turns: AlwaysStoppedAnimation(store.state.rotation / 360.0),
-                child: Image.network(
-                  url,
-                  //fit: BoxFit.cover,
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Opacity(
+          opacity: lifecycle == AppLifecycleState.resumed ? 1.0 : 0.0,
+          child: Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                color: Colors.black.withAlpha(100),
+                spreadRadius: 10,
               ),
-            ),
-          ],
+            ]),
+            child: Center(child: Image.asset("assets/card.jpg")),
+          ),
         ),
       ),
     );
