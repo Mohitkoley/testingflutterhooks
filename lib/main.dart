925f05f66b28e0f7898eaa4f7b4fce7fcdf7ce27
Mohit <mohitkoley5@gmail.com>
2022-06-24 14:53:05 +0530

    streaming

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -20,7 +20,7 @@ extension Normalize on num {
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const HomePage(),
+    home: HomePage(),
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
@@ -28,63 +28,43 @@ void main() {
 }
 
 const String url = "https://images8.alphacoders.com/124/thumb-1920-1242638.jpg";
-const imageHeight = 300.0;
 
 class HomePage extends HookWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  late final StreamController<double> streamController =
+      useStreamController<double>(onListen: () {
+    streamController.sink.add(0.0);
+  });
 
   @override
   Widget build(BuildContext context) {
-    final opacity = useAnimationController(
-        initialValue: 1.0, lowerBound: 0.0, upperBound: 1.0);
-
-    final size = useAnimationController(
-        initialValue: 1.0, lowerBound: 0.0, upperBound: 1.0);
-
-    final controller = useScrollController();
-
-    useEffect(() {
-      controller.addListener(() {
-        final newOpacity = max(imageHeight - controller.offset, 0.0);
-        final normalized = newOpacity.normalize(0.0, imageHeight).toDouble();
-        opacity.value = normalized;
-        size.value = normalized;
-      });
-      return null;
-    }, [controller]);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Hook Example"),
         centerTitle: true,
       ),
-      body: Center(
-          child: Column(
-        children: [
-          SizeTransition(
-            sizeFactor: size,
-            axis: Axis.vertical,
-            axisAlignment: -1.0,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                url,
-                height: imageHeight,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: controller,
-              itemCount: 100,
-              itemBuilder: (context, index) => ListTile(
-                title: Text("Person ${index + 1}"),
-              ),
-            ),
-          )
-        ],
-      )),
+      body: StreamBuilder<double>(
+          stream: streamController.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              final rotation = snapshot.data ?? 0.0;
+              return GestureDetector(
+                onTap: () => streamController.sink.add(rotation + 0.1),
+                child: RotationTransition(
+                  turns: AlwaysStoppedAnimation(rotation / 360.0),
+                  child: Center(
+                    child: Image.network(
+                      url,
+                      //fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            }
+          }),
     );
   }
 }
